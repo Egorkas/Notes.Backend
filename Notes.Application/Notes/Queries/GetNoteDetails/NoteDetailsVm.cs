2720f14b3cf32324a9f2676c18040736820cf3c2
Egorkas <egorkaras1997@gmail.com>
2022-05-16 23:41:26 +0300

    Delete unnecessary "using"

--- Notes.Application/Notes/Queries/GetNoteDetails/NoteDetailsVm.cs
+++ Notes.Application/Notes/Queries/GetNoteDetails/NoteDetailsVm.cs
@@ -2,10 +2,6 @@
 using Notes.Application.Common.Mappings;
 using Notes.Domain;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Notes.Application.Notes.Queries.GetNoteDetails
 {
